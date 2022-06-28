a598a29bfc38d5d710e0ed8f9a6403a5f67cc627
Kalyani <yewalekalyani94@gmail.com>
2022-06-28 15:52:20 -0700

    Update

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,14 @@ module.exports = {
   ci: {
     collect: {
 		  startServerCommand: 'npm run start',
+		  numberOfRuns: 5,
           url: ['http://localhost:8080']
+    },
+	 assert: {
+      assertions: {
+        'categories:performance': ['warn', {minScore: 1}],
+        'categories:accessibility': ['error', {minScore: 1}]
+      }
     },
     upload: {
        target: 'temporary-public-storage',
